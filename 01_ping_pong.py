eb36f00c8cece0b5dc4febe22495007de13e8360
KJS2022 <jinsonkong@gmail.com>
2025-08-24 14:34:45 +0800

    commit

--- 01_ping_pong.py
+++ 01_ping_pong.py
@@ -20,3 +20,20 @@ BASE_SPEED_PADDLE = 6
 BALL_SPEED_X = 4
 BALL_SPEED_Y = 3
 
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                game = False
+            if e.key == K_p:
+                paused = not paused
+            if e.key == K_r:
+                score1 = score2 = 0
+                winner = None
+                racket1.rect.centery = win_height //2
+                racket2.rect.centery = win_height //2
+                ball.center_serve(direction=1)
+    
